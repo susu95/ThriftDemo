1340e6722f79c4635386ee8847758f3e68551669
shilifang <shilifang@meituan.com>
2016-06-17 16:40:50 +0800

    ddddd

---
+++ ThriftTestDemo/src/main/java/service/demo/HelloImpl.java
@@ -0,0 +1,14 @@
+package service.demo;
+
+import org.apache.thrift.TException;
+
+public class HelloImpl implements Hello.Iface {
+
+        public String helloString(String para)  throws TException { return null; }
+    public int helloInt(int para)  throws TException { return null; }
+    public boolean helloBoolean(boolean para)  throws TException { return null; }
+    public void helloVoid()  throws TException { return null; }
+    public String helloNull()  throws TException { return null; }
+
+
+}
